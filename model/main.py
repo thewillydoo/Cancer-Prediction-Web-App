cdb7561edd8f40dbf6e9a819fbcd69db2726d987
William Luo <william2299@gmail.com>
2024-02-10 10:39:30 -0700

    imported libraries

---
+++ model/main.py
@@ -0,0 +1,10 @@
+import numpy as np 
+import pandas as pd
+import seaborn as sns
+import sklearn
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+
