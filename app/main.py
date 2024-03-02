a78d0bf9777dd47542b1cfad8c63d8025a157f42
William Luo <william2299@gmail.com>
2024-03-02 16:28:23 -0700

    solved plotly pylance issue

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import pickle
+import plotly.graph_objects as go
 
 def get_clean_data():
     # Load the data
@@ -65,6 +66,35 @@ def add_sidebar():
     
     return input_dict
 
+def get_radar_chart(input_data):
+    categories = ['processing cost','mechanical properties','chemical stability',
+              'thermal stability', 'device integration']
+
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatterpolar(
+        r=[1, 5, 2, 2, 3],
+        theta=categories,
+        fill='toself',
+        name='Product A'
+    ))
+    fig.add_trace(go.Scatterpolar(
+        r=[4, 3, 2.5, 1, 2],
+        theta=categories,
+        fill='toself',
+        name='Product B'
+    ))
+
+    fig.update_layout(
+    polar=dict(
+        radialaxis=dict(
+        visible=True,
+        range=[0, 5]
+        )),
+    showlegend=False
+    )
+
+
 def main():
     st.set_page_config(
         page_title="Breast Cancer Prediction App",
@@ -83,7 +113,7 @@ def main():
     col1, col2 = st.columns([4, 1])
 
     with col1:
-        st.write("this is col1")
+        get_radar_chart(input_data)
     with col2:
         st.write("this is col2")
 
